888f3d8a46bf7a60477b0ac05ec6eb6b41e997ce
An Pham <regod@VN-LW13551-M.local>
2022-05-26 18:26:21 +0700

    expose type

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './req2burp';
 export { burpToRequest } from './burp2req';
+export * from './type';
--- src/type.ts
+++ src/type.ts
@@ -10,28 +10,6 @@ export type Method =
   | 'link' | 'LINK'
   | 'unlink' | 'UNLINK';
 
-export type ResponseType =
-  | 'arraybuffer'
-  | 'blob'
-  | 'document'
-  | 'json'
-  | 'text'
-  | 'stream';
-
-export type responseEncoding =
-  | 'ascii' | 'ASCII'
-  | 'ansi' | 'ANSI'
-  | 'binary' | 'BINARY'
-  | 'base64' | 'BASE64'
-  | 'base64url' | 'BASE64URL'
-  | 'hex' | 'HEX'
-  | 'latin1' | 'LATIN1'
-  | 'ucs-2' | 'UCS-2'
-  | 'ucs2' | 'UCS2'
-  | 'utf-8' | 'UTF-8'
-  | 'utf8' | 'UTF8'
-  | 'utf16le' | 'UTF16LE';
-
 export interface AxiosRequestConfig {
   httpVersion?: string;
   url?: string;
